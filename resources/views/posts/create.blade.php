8cc046ae441f7890538a99e0ebb28be775a07da0
Alvaro <alvaro96rojo@gmail.com>
2023-03-13 23:35:05 +0100

    Create views

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layout.web-layout')
+
+@section('content')
+
+<div class="row">
+	<div class="col-lg-2"></div>
+	<div class="col-lg-8 my-4">
+		<div class="float-start">
+			<h4>Add New Post</h4>
+		</div>
+		<div class="float-end">
+			<a class="btn btn-sm btn-warning" href="{{ route('posts.index') }}"><i class="fa fa-chevron-left"></i> Back</a>
+		</div>
+	</div>
+	<div class="col-lg-2"></div>
+</div>
+
+<div class="row">
+	<div class="col-lg-2"></div>
+	<div class="col-lg-8">
+		<form method="post" action="{{ route('posts.store') }}">
+			@csrf
+			<div class="mb-3">
+				<label class="form-label fw-bold">Title</label>
+				<input type="text" class="form-control" placeholder="Enter Title" name="title" required>
+			</div>
+			<div class="mb-3">
+				<label class="form-label fw-bold">Description</label>
+				<textarea class="form-control" placeholder="Description..." name="description" required></textarea>
+			</div>
+			<div class="mb-3">
+				<button type="Save" class="btn btn-sm btn-success">
+					<i class="fa fa-floppy-o"></i> Submit
+				</button>
+			</div>
+		</form>
+	</div>
+	<div class="col-lg-2"></div>
+</div>
+
+@endsection
